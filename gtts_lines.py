82d8d3c6028ce004349e70ab483bc1847c6fb5e1
Rodrigo Github <git@hub.com>
2023-12-14 15:05:42 -0300

    Update 🏴‍☠️

--- gtts_lines.py
+++ gtts_lines.py
@@ -24,6 +24,7 @@ import logging
 import os
 import re
 import sys
+
 from gtts import gTTS
 from unidecode import unidecode
 
@@ -42,20 +43,18 @@ class TextTransliterator:
 
     @staticmethod
     def remove_special_chars(file_name):
-        return re.sub(r'[^a-zA-Z0-9 ]', '_', file_name)
+        reg = re.sub(r'[^a-zA-Z0-9 ]', '_', file_name)  # no accents
+        reg = re.sub(r'_{2,}', '_', reg)  # multiple _
+        return re.sub(r'^_+|_+$', '', reg)  # strip _
 
-    def transliterate_text(self, text):
-        if text == '':
-            print('Error: line cannot be empty!')
-            sys.exit()
-
+    def transliterate_text(self, text, line_count):
         file_name = self.remove_accents(text)
-        file_name = self.remove_special_chars(file_name) + '.mp3'
+        file_name = self.remove_special_chars(file_name)[:30] + '.mp3'
         self.create_mp3(text, file_name)
 
     def create_mp3(self, text, file_name):
         tts = gTTS(text=text, lang=self.language, slow=self.slow)
-        tts.save(f'./mp3/{file_name}')
+        tts.save(f'./mp3/{line_count}_{file_name}')
 
 
 if __name__ == "__main__":
@@ -68,6 +67,9 @@ if __name__ == "__main__":
     with open(file_name_input, 'r', encoding='utf8') as file:
         lines = file.readlines()
         for line in lines:
+            line = line.strip()  # strip spaces
+            if not line or line.startswith('#'):
+                continue  # Ignore whitespaces and comment lines
+            print(line)
             line_count += 1
-            print(line.strip())
-            transliterator.transliterate_text(line.strip())
+            transliterator.transliterate_text(line.strip(), line_count)
